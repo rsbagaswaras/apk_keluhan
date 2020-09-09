a57d7ce0dd0065e3e224b8b97ac1dd5b88d13ef4
zahwa-1500 <sasa123.178.s1@gmail.com>
2020-09-09 13:41:28 +0700

    press back

--- app/src/main/java/com/example/form_keluhan/LoginActivity.java
+++ app/src/main/java/com/example/form_keluhan/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private GoogleSignInOptions gso;
 
+    private boolean doubleBacktoExit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -135,4 +137,22 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBacktoExit){
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBacktoExit = true;
+        Toast.makeText(this, "Tekan Lagi Untuk Keluar",Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBacktoExit = false;
+            }
+        }, 2000);
+    }
+
 }
