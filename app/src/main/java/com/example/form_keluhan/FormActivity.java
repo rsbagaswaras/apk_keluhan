841109257a78075de793c328c42a2bf3a05622aa
nisrinatata <56124010+nisrinatata@users.noreply.github.com>
2020-09-23 07:35:36 -0700

    namrespon

--- app/src/main/java/com/example/form_keluhan/FormActivity.java
+++ app/src/main/java/com/example/form_keluhan/FormActivity.java
@@ -21,8 +21,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -39,6 +42,8 @@ public class FormActivity extends AppCompatActivity  {
     Button btn_add;
     EditText edt_nama, edt_ruangan, edt_keluhan;
     Spinner spinner;
+    TextView txt_respon;
+
 
     private Uri pickedImgUri = null;
     private static final int PReqCode = 2 ;
@@ -51,7 +56,12 @@ public class FormActivity extends AppCompatActivity  {
 
         imageClick();
 
+        txt_respon = findViewById(R.id.txt_respon);
 
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount != null){
+            txt_respon.setText(signInAccount.getDisplayName());
+        }
 
     }
 
@@ -140,7 +150,7 @@ public class FormActivity extends AppCompatActivity  {
     public void Click3(View view) {
 
         //deklarasi
-        edt_nama = findViewById(R.id.edt_nama);
+        txt_respon = findViewById(R.id.txt_respon);
         edt_ruangan = findViewById(R.id.edt_ruangan);
         edt_keluhan = findViewById(R.id.edt_keluhan);
         spinner = findViewById(R.id.spinner);
@@ -164,7 +174,7 @@ public class FormActivity extends AppCompatActivity  {
             DatabaseReference newmyRef = myRef.push();
 
             //memberi nilai pada referensi yang dituju
-            newmyRef.child("Nama").setValue(edt_nama.getText().toString());
+          //  newmyRef.child("Nama").setValue(edt_nama.getText().toString());
             newmyRef.child("Ruangan").setValue(edt_ruangan.getText().toString());
             newmyRef.child("Keluhan").setValue(edt_keluhan.getText().toString());
 
@@ -172,7 +182,7 @@ public class FormActivity extends AppCompatActivity  {
         Toast.makeText(getApplicationContext(), "Data Keluhan Sudah Tersimpan", Toast.LENGTH_SHORT).show();
 
         //mengosongkan isian setelah klik button upload
-        edt_nama.setText("");
+        txt_respon.setText("");
         edt_ruangan.setText("");
         edt_keluhan.setText("");
         edt_keluhan.setText("");
