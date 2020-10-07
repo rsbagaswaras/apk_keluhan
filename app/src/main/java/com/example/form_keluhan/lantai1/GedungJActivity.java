28d843c979e9de67f83b0d3cc0444003468b8568
Lathifah1803 <lthfhhhhh2@gmail.com>
2020-10-07 09:11:09 +0700

    Coordinat

--- app/src/main/java/com/example/form_keluhan/lantai1/GedungJActivity.java
+++ app/src/main/java/com/example/form_keluhan/lantai1/GedungJActivity.java
@@ -3,6 +3,7 @@ package com.example.form_keluhan.lantai1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.form_keluhan.ClickableArea;
 import com.example.form_keluhan.ClickableAreasImage;
@@ -40,12 +41,98 @@ public class GedungJActivity extends AppCompatActivity implements OnClickableAre
     public void onClickableAreaTouched(Object item) {
         if (item instanceof State) {
             String text = ((State) item).getName();
-
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
             switch (text) {
-                case "GUDANG CAIRAN":
-                    Intent K = new Intent(GedungJActivity. this, FormActivity.class);
-                    startActivity(K);
+                case "LOKER PETUGAS":
+                    Intent intent = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent);
                     break;
+
+                    case "ICU INFEKSIUS":
+                     Intent intent1 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent1);
+                    break;
+
+                    case "R.TRANSISI":
+                     Intent intent2 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent2);
+                    break;
+
+                    case "NURSE STATION":
+                      Intent intent3 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent3);
+                    break;
+
+                    case "HALL":
+                     Intent intent4 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent4);
+                    break;
+
+                    case "LOKER UMUM":
+                     Intent intent5 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent5);
+                    break;
+
+                    case "LOKER PETUGAS 1":
+                     Intent intent6 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent6);
+                    break;
+
+                    case "R.OBAT":
+                    Intent intent7 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent7);
+                    break;
+
+                    case "R.ALAT & LINEN":
+                     Intent intent8 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent8);
+                    break;
+
+                    case "R.PERAWAT":
+                     Intent intent9 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent9);
+                    break;
+
+                    case "PANTRY":
+                      Intent intent10 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent10);
+                    break;
+
+                    case "SPOTCHECK":
+                      Intent intent11 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent11);
+                    break;
+
+
+                    case "LAV":
+                    Intent intent12 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent12);
+                    break;
+
+
+                    case "LAV 1":
+                    Intent intent13 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent13);
+                    break;
+
+
+                    case "NICU":
+                    Intent intent14 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent14);
+                    break;
+
+                    case "PICU":
+                    Intent intent15 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent15);
+                    break;
+
+                    case "NURSE INFEKSIUS STATION":
+                    Intent intent16 = new Intent(GedungJActivity. this, FormActivity.class);
+                    startActivity(intent16);
+                    break;
+
+
+
 
             }
         }
@@ -55,7 +142,27 @@ public class GedungJActivity extends AppCompatActivity implements OnClickableAre
 
         List<ClickableArea> clickableAreas = new ArrayList<>();
 
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("LOKER PETUGAS")));
         clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+        clickableAreas.add(new ClickableArea(0, 0, 1840, 2684, new State("GUDANG CAIRAN")));
+
+
+
+
         return clickableAreas;
     }
 
