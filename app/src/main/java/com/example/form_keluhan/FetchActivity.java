bdd3d75b542edd6ff6cb5f80e7551105313c4196
zahwa-1500 <sasa123.178.s1@gmail.com>
2020-10-31 06:43:16 +0700

    logout

--- app/src/main/java/com/example/form_keluhan/FetchActivity.java
+++ app/src/main/java/com/example/form_keluhan/FetchActivity.java
@@ -23,37 +23,11 @@ import java.util.ArrayList;
 
 public class FetchActivity extends AppCompatActivity {
 
-    DatabaseReference reference;
-    RecyclerView recyclerView;
-    ArrayList<Form> list;
-    FetchAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fetch);
-        recyclerView = (RecyclerView) findViewById(R.id.myRecycler);
-        recyclerView.setLayoutManager( new LinearLayoutManager(this));
-
-
-        reference = FirebaseDatabase.getInstance().getReference().child("Keluhan");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                list = new ArrayList<Form>();
-                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
-                {
-                    Form f = dataSnapshot1.getValue(Form.class);
-                    list.add(f);
-                }
-                adapter = new FetchAdapter(FetchActivity.this,list);
-                recyclerView.setAdapter(adapter);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(FetchActivity.this, "Opsss.... Something is wrong", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
