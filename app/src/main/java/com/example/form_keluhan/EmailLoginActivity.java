b2f3ee8f55f728cfb29e96f2752ce824ace67302
zahwa-1500 <sasa123.178.s1@gmail.com>
2020-10-31 08:19:36 +0700

    hide/show password admin

--- app/src/main/java/com/example/form_keluhan/EmailLoginActivity.java
+++ app/src/main/java/com/example/form_keluhan/EmailLoginActivity.java
@@ -7,6 +7,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +22,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import cyd.awesome.material.AwesomeText;
+import cyd.awesome.material.FontCharacterMaps;
+
 public class EmailLoginActivity extends AppCompatActivity {
 
     private EditText userMail, userPassword;
@@ -29,6 +33,8 @@ public class EmailLoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Intent Lantai1Activity;
     private ImageView loginPhoto;
+    private AwesomeText awesomeText;
+    private boolean pwd_status = true;
 
 
 
@@ -45,17 +51,6 @@ public class EmailLoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         Lantai1Activity = new Intent(this, Lantai1Activity.class);
         loginPhoto = findViewById(R.id.login_photo);
-        loginPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(registerActivity);
-                finish();
-
-
-            }
-        });
 
         loginProgress.setVisibility(View.INVISIBLE);
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +71,24 @@ public class EmailLoginActivity extends AppCompatActivity {
                 }
 
 
+            }
+        });
+
+        awesomeText = findViewById(R.id.awesome);
+        awesomeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pwd_status){
+                    userPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    pwd_status = false;
+                    awesomeText.setMaterialDesignIcon(FontCharacterMaps.MaterialDesign.MD_VISIBILITY);
+                    userPassword.setSelection(userPassword.length());
+                }else {
+                    userPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+                    pwd_status = true;
+                    awesomeText.setMaterialDesignIcon(FontCharacterMaps.MaterialDesign.MD_VISIBILITY_OFF);
+                    userPassword.setSelection(userPassword.length());
+                }
             }
         });
 
