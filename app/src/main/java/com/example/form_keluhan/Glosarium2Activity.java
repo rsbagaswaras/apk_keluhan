e5ddbbb44d560ef07b72507ec7d880818ef732f8
alfiaaish <alfia02s4@gmail.com>
2020-11-01 10:19:11 +0700

    glosarium2

---
+++ app/src/main/java/com/example/form_keluhan/Glosarium2Activity.java
@@ -0,0 +1,14 @@
+package com.example.form_keluhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Glosarium2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glosarium2);
+    }
+}
