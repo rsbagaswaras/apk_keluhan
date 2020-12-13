2eed1aaa6a78af5d83eee6e975b5336338eb4fb0
Lathifah1803 <lthfhhhhh2@gmail.com>
2020-12-13 09:11:44 +0700

    Coment CRUD Fetching

--- app/src/main/java/com/example/form_keluhan/Fetching/FetchAdapter.java
+++ app/src/main/java/com/example/form_keluhan/Fetching/FetchAdapter.java
@@ -44,6 +44,7 @@ public class FetchAdapter extends FirebaseRecyclerAdapter<Form,FetchAdapter.myvi
         holder.keluhan.setText(form.getKeluhan());
         Glide.with(holder.img.getContext()).load(form.getPicture()).into(holder.img);
 
+        //Agar button edit bisa diintent
             holder.edit.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -51,7 +52,7 @@ public class FetchAdapter extends FirebaseRecyclerAdapter<Form,FetchAdapter.myvi
                             .setContentHolder(new ViewHolder(R.layout.dialogcontent))
                             .setExpanded(true,1100)
                             .create();
-
+            //Proses saat edit text pada dialogcontent.xml bisa muncul
                 View myview=dialogPlus.getHolderView();
                     final EditText tgl_penyampaian=myview.findViewById(R.id.tanggal_penyampaian);
                     final EditText nama_responden=myview.findViewById(R.id.nama_responden);
@@ -62,14 +63,17 @@ public class FetchAdapter extends FirebaseRecyclerAdapter<Form,FetchAdapter.myvi
                     EditText tanggal_dirubah=myview.findViewById(R.id.datedeparture);
                     Button submit=myview.findViewById(R.id.usubmit);
 
+                    //Proses pemanggilan data pada card view dibagian form.java dengan menggunakaan getter setter nya
                     tgl_penyampaian.setText(form.getTanggal_penyampaian());
                     nama_responden.setText(form.getNama_responden());
                     ruangan.setText(form.getRuangan());
                     kategori.setText(form.getKategori());
                     keluhan.setText(form.getKeluhan());
 
-
+                //dialogcontent.xml berhasil dimunculkan
                     dialogPlus.show();
+
+                    //Proses agar button update bisa diintenkan
                         submit.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -80,6 +84,8 @@ public class FetchAdapter extends FirebaseRecyclerAdapter<Form,FetchAdapter.myvi
                                 map.put("kategori",kategori.getText().toString());
                                 map.put("keluhan",keluhan.getText().toString());
 
+                               //Data yang sudah diedit lalu berhasil di update akan otomatis berubah pada card view di fetching data
+                                // dan juga pada firebase (data akan diperbaharui)
                                 FirebaseDatabase.getInstance().getReference().child("RSBW_KELUHAN")
                                         .child(getRef(position).getKey()).updateChildren(map)
                                         .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -101,13 +107,14 @@ public class FetchAdapter extends FirebaseRecyclerAdapter<Form,FetchAdapter.myvi
                 }
             });
 
+        //Proses Agar button delete bisa diklik
             holder.delete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     AlertDialog.Builder builder=new AlertDialog.Builder(holder.img.getContext());
                     builder.setTitle("Delete Data");
                     builder.setMessage("Delete....?");
-
+                //Proses saat pengguna mengklik button YES maka data akan terhapus pada cardview difetching data dan firebase
                     builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
@@ -115,7 +122,7 @@ public class FetchAdapter extends FirebaseRecyclerAdapter<Form,FetchAdapter.myvi
                                     .child(getRef(position).getKey()).removeValue();
                         }
                     });
-
+                //Proses jika admin tidak jadi menghapus data maka akan kembali pada tambilan fetching data
                     builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
@@ -137,6 +144,7 @@ public class FetchAdapter extends FirebaseRecyclerAdapter<Form,FetchAdapter.myvi
 
     class myviewholder extends RecyclerView.ViewHolder{
 
+        //Deklarasi
         ImageView img,edit,imgg,delete;
         EditText myCalender;
         TextView tgl_penyampaian, nama_responden, ruangan, kategori, keluhan;
